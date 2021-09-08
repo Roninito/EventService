ce7d2af2e3cb3522ca8ea38c8057159c4353002f
Leerie Simpson <leerie.a.simpson@gmail.com>
2021-09-08 16:42:31 -0400

    Updated Package dependencies

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Roninito/ServiceKit.git", from: "0.2.3")
+        .package(url: "https://github.com/Roninito/ServiceKit.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
