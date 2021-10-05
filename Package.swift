a6868a9239cd68ee1914e57ab8a62ac86a55d4bb
Leerie Simpson <leerie.a.simpson@gmail.com>
2021-10-05 02:50:27 -0400

    Package update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EventService",
-    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v7)],
+    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Roninito/ServiceKit.git", from: "1.0.0")
+        .package(url: "https://github.com/Roninito/ServiceKit.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
