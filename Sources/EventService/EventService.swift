7b09b6a0a030dbfa303160f46d29b1fcb604344d
Leerie Simpson <leerie.a.simpson@gmail.com>
2021-10-06 11:34:48 -0400

    Events now have their own OperationQueues and are now multithreaded.

--- Sources/EventService/EventService.swift
+++ Sources/EventService/EventService.swift
@@ -15,15 +15,24 @@ public class Events: NSObject, ObservableObject, Serviceable, NSSecureCoding {
     public typealias ServiceProvider = Events
     public var provider: Events { self }
     private var manifest: [String: Event] = [:]
-    @Published public var announcement: String = "Event Service Ready"
 	
+	private lazy var queue: OperationQueue = {
+		let q = OperationQueue()
+		q.name = identifier
+		q.qualityOfService = .default
+		return q
+	}()
+	
+    @Published public var announcement: String = "Event Service Ready"
+	private var identifier: String = "Generic Event Service"
 	
 	public var numberOfRegisteredEvents: Int { manifest.count }
 	public var registeredEvents: [String] { manifest.keys.sorted() }
     
     
-	public init(isService: Bool = true) {
+	public init(identifier: String?, isService: Bool = true) {
 		super.init()
+		self.identifier = identifier ?? self.identifier
 		if isService == true { registerAsServiceable() }
 	}
     
@@ -39,7 +48,10 @@ public class Events: NSObject, ObservableObject, Serviceable, NSSecureCoding {
 	
     
     private func register(event: Event) {
-        manifest[event.id] = event
+		queue.addOperation { [unowned self] in
+			manifest[event.id] = event
+		}
+        
     }
     
     
@@ -50,12 +62,18 @@ public class Events: NSObject, ObservableObject, Serviceable, NSSecureCoding {
     
     public subscript(_ key: String = "") -> Event? {
         get { event(key) }
-        set { register(event: newValue!) }
+        set {
+			queue.addOperation { [unowned self] in
+				register(event: newValue!)
+			}
+		}
     }
     
     
     public func raise(by: Any, _ eventID: String, info: [String: Any]) {
-        announcement = "Event: \(eventID) raised by: \(String(describing: by)) with info: \(info)"
-        event(eventID)?.raised = info
+		queue.addOperation { [unowned self] in
+			announcement = "Event: \(eventID) raised by: \(String(describing: by)) with info: \(info)"
+			event(eventID)?.raised = info
+		}
     }
 }
